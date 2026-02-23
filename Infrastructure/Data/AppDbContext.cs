bcf9f1b792ec30c4ca677e24668691f1058edcf5
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2026-02-23 23:05:12 +0200

    Add files via upload

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Application.Common.Interfaces;
+using Domain.Todos;
+using Infrastructure.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+: DbContext(options), IAppDbContext
+{
+    public DbSet<Todo> Todos => Set<Todo>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoConfiguration).Assembly);   
+    }
+}
