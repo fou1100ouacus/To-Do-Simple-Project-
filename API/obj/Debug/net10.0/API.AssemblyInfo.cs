258f2438d231a9425e3981aea9eb993a3fce4d74
“Aya” <engayamohamed23@gmail.com>
2026-02-24 16:17:03 +0200

    commmit

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5651bf641567a26beb9e471ae08e12de613001")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f21423a622a317629e2e26408493d65a6ec248")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
